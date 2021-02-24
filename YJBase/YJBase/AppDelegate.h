af000e14dcf5a7090073d371ad2defc3bf8c32c5
lyj <liyj@qcgelei.com>
2021-02-24 16:24:03 +0800

    创建

---
+++ YJBase/YJBase/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  YJBase
+//
+//  Created by lyj on 2021/2/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ YJBase/YJBaseTests/YJBaseTests.m
@@ -0,0 +1,36 @@
+//
+//  YJBaseTests.m
+//  YJBaseTests
+//
+//  Created by lyj on 2021/2/24.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YJBaseTests : XCTestCase
+
+@end
+
+@implementation YJBaseTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
